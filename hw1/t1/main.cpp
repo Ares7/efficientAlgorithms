4d309c177bb455c5330e799a511e0b18893afb2c
Sergey Nasonov <sergey.nasonov@tum.de>
2017-04-29 15:35:24 +0200

    finished task#1.

--- hw1/t1/main.cpp
+++ hw1/t1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ios::sync_with_stdio(false);
+
+    int k = 0;
+    cin>>k;
+
+    vector<string> results;
+    string line;
+
+    for (int i = 0; i < k; ++i)
+    {
+        cin>> line;
+        results.push_back( line );
+    }
+
+    for (int i = 0; i < k; ++i) {
+        cout << "Case #" <<i+1<<": Hello " << results[i] << "!"<<endl;
+    }
+
     return 0;
 }
