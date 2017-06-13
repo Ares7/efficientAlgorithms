a93ba3684b5f1a33a4c9e5b3f96460153745a74b
Sergey Nasonov <sergey.nasonov@tum.de>
2017-06-13 09:45:05 +0200

    finished A: basic case.

--- hw6/A/main.cpp
+++ hw6/A/main.cpp
@@ -1,68 +1,76 @@
 //Partially adapted from www.geeksforgeeks.org
-#define N 4
-#include<stdio.h>
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-//int N;
+int N;
 int en = 0;
 int statedN = 0;
-void printSolution(char board[N][N])
-{
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            printf(" %d ", board[i][j]);
-        printf("\n");
-    }
-}
 
-bool isSafe(char board[N][N], int row, int col)
+typedef struct ans
+{
+    int an;
+    string imp;
+    char arr_out[15][15];
+}ans1;
+
+vector<ans1> v;
+
+bool isSafe(char** board, int row, int col)
 {
     int i, j;
 
-
+    //mode = 1 is operational
+    // mode = 0 is inital check
+    if(board[row][col] == 'x' )
+        return true;
     /* Check this row on left side */
     for (i = 0; i < col; i++)
         if (board[row][i]== '1' || board[row][i] == 'x')
             return false;
 
     /* Check this row on right side */
-    for (i = col; i < N; i++)
+    for (i = col+1; i < N; i++)
         if (board[row][i]== '1' || board[row][i] == 'x')
             return false;
 
     /* Check upper diagonal on left side */
-    for (i=row, j=col; i>=0 && j>=0; i--, j--)
-        if (board[i][j] == '1' || board[row][i] == 'x')
+    for (i=row-1, j=col-1; i>=0 && j>=0; i--, j--)
+        if (board[i][j] == '1' || board[i][j] == 'x')
             return false;
 
     /* Check lower diagonal on left side */
-    for (i=row, j=col; j>=0 && i<N; i++, j--)
-        if (board[i][j] == '1' || board[row][i] == 'x')
+    for (i=row+1, j=col-1; j>=0 && i<N; i++, j--)
+        if (board[i][j] == '1' || board[i][j] == 'x')
             return false;
 
     //chk the presence of a permanent node of the right side:
 
     /* Check upper diagonal on right side */
-    for (i=row, j=col; i>=0 && j<N; i--, j++)
-        if (board[i][j] == '1' || board[row][i] == 'x')
+    for (i=row-1, j=col+1; i>=0 && j<N; i--, j++)
+        if (board[i][j] == '1' || board[i][j] == 'x')
             return false;
 
     /* Check lower diagonal on right side */
-    for (i=row, j=col; j>N && i<N; i++, j++)
-        if (board[i][j] == '1' || board[row][i] == 'x')
+    for (i=row+1, j=col+1; j<N && i<N; i++, j++)
+        if (board[i][j] == '1' || board[i][j] == 'x')
             return false;
 
+    /* Check TOP and BOT*/
+    for (i=0; i<N ; i++)
+    {
+        if (board[i][col] == 'x' && i!=row)
+        {
+            return false;
+        }
+    }
 
     return true;
 }
 
-/* A recursive utility function to solve N
-   Queen problem */
-bool solveNQUtil(char board[N][N], int col)
+bool solveNQUtil(char** board, int col)
 {
     /* base case: If all queens are placed
       then return true */
@@ -77,35 +85,37 @@ bool solveNQUtil(char board[N][N], int col)
         if ( isSafe(board, i, col) )
         {
             /* Place this queen in board[i][col] */
-            board[i][col] = '1';
-            statedN++;
+            if(board[i][col] != 'x')
+            {
+                board[i][col] = '1';
+                statedN++;
+            }
+
             /* recur to place rest of the queens */
             if ( solveNQUtil(board, col + 1) )
                 return true;
 
             /* If placing queen in board[i][col]
                doesn't lead to a solution, then
-               remove queen from board[i][col] */
-            board[i][col] = '0'; // BACKTRACK
-            statedN--;
+               remove queen from board[i][col]
+               and continue with next i (row) */
+            if(board[i][col] != 'x')
+            {
+                board[i][col] = '0';
+                statedN--;
+            }
+             // BACKTRACK
+
         }
     }
 
-    /* If queen can not be place in any row in
-       this colum col  then return false */
-    if(statedN + en == N)
-    {
-        return true;
-    }
     return false;
 }
 
 
-// driver program to test above function
 int main()
 {
 
-    //TODO: do we have to chk for top and bottom for x?
     int t = 0;
     cin>>t;
 
@@ -113,122 +123,106 @@ int main()
     {
         int n;
         cin >> n;
-        //N = n;
-        char board[N][N] ;
+        N = n;
+
+        ans aw;
+        aw.an = n;
 
 
-        int inpt;
+        char** board = new char*[N];
 
+        for (int i = 0; i < n; i++)
+        {
+            board[i] = new char [N];
+            for (int j = 0; j < n; j++)
+            {
+                cin >> board[i][j];
+                if (board[i][j] == 'x')
+                    en++;
+            }
+        }
+
+        if(n==2 || n==3)
+        {
+            aw.imp = "impossible";
+            v.push_back(aw);
+            continue;
+        }
 
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                cin >> inpt;
-                if (inpt == 'x')
-                    en++;
-                board[i][j] = inpt;
-
+//                if (isSafe(board, i, j, 0) == false && n!=1)
+//                {
+//                    aw.imp = "impossible";
+//                }
             }
         }
 
 
-
-        if ( solveNQUtil(board, 0) == false )
+        if ( solveNQUtil(board, 0) == false && n!=1)
         {
-            cout<<"Solution does not exist";
-            return false;
+            aw.imp = "impossible";
+        }
+        else if (aw.imp != "impossible")
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    aw.arr_out[i][j] = board[i][j];
+                }
+
+                delete[] board[i];
+            }
+            delete[] board;
+
+        }
+
+        v.push_back(aw);
+
+    }
+
+
+    for (int l = 0; l < v.size(); ++l)
+    {
+        cout<<"Case #" << l+1<<":";
+        cout<<endl;
+
+        if(v[l].imp.length()>0 )
+        {
+            cout<<v[l].imp<<endl;
         }
         else
         {
-            printSolution(board);
+            for (int i = 0; i < v[l].an; i++)
+            {
+                for (int j = 0; j < v[l].an; j++)
+                {
+                    if(v[l].arr_out[i][j] == '1')
+                    {
+                        cout<<'x';
+                    }
+                    else if(v[l].arr_out[i][j] == '0')
+                    {
+                        cout<<'.';
+                    }
+                    else
+                    {
+                        cout<<v[l].arr_out[i][j];
+                    }
+
+                }
+
+
+                cout<<endl;
+            }
+
         }
 
     }
 
 
-
-
-
-
     return 0;
 }
-
-
-////
-////  main.cpp
-////  A
-////
-////  Created by Sergey Nasonov on 08/06/2017.
-////  Copyright © 2017 Sergey Nasonov. All rights reserved.
-////
-//
-////15: 285053
-//
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main(int argc, const char * argv[]) {
-//    // insert code here...
-//
-//    int t = 0;
-//    cin>>t;
-//    for (int k = 0; k<t; k++)
-//    {
-//        int n;
-//        char arr[ 15 ][ 15 ];
-//        cin>> n;
-//
-//        for (int i = 0; i<n; i++)
-//        {
-//            for (int j = 0; j<n; j++)
-//            {
-//                cin>> arr[i][j];
-//
-//            }
-//        }
-//
-//        //run the entire array again bc inpur overwrides the markings.
-//        for (int i = 0; i<n; i++)
-//        {
-//            for (int j = 0; j < n; j++)
-//            {
-//                if(arr[i][j]=='x')
-//                {
-//                    //mark the row as filled:
-//                    for (int r = 0; r<n; r++)
-//                    {
-//                        if(j != r  && arr[i][r]!='x')
-//                            arr[i][r] = 'f';
-//                    }
-//
-//                    //mark the col as filled:
-//                    for (int c = 0; c<n; c++)
-//                    {
-//                        if(i != c  && arr[c][j]!='x')
-//                            arr[c][j] = 'f';
-//                    }
-//
-//                    //mark the diagonals.
-//
-//
-//                }
-//
-//            }
-//        }
-//
-//
-//        for (int i = 0; i<n; i++)
-//        {
-//            for (int j = 0; j < n; j++)
-//            {
-//                cout << arr[i][j] << " ";
-//            }
-//            cout << endl;
-//        }
-//
-//    }
-//
-//    return 0;
-//}
